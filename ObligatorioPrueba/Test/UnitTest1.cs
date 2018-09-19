0817ea7b1571a9cfb5a054e7687a0d20dac10a88
laual <laual@DESKTOP-14HJECE>
2018-09-19 20:04:53 -0300

    [red] add user test

---
+++ ObligatorioPrueba/Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddUser()
+        {
+            User user = new User()
+            {
+                Name = "Victor",
+            };
+            Logic.AddUser(user);
+            Assert.IsTrue(Logic.GetUsers().Exists(user));
+        }
+    }
+}
